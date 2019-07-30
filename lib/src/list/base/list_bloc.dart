9629ef5784eee67d802bb54f909750ca21e03522
klisiewicz <karol.lisiewicz@gmail.com>
2019-07-30 17:39:32 +0200

    ListBloc extends FilterListBloc with void filter.

--- lib/src/list/base/list_bloc.dart
+++ lib/src/list/base/list_bloc.dart
@@ -1,60 +1,21 @@
-import 'dart:collection';
-
-import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc_patterns/src/list/base/list_events.dart';
-import 'package:flutter_bloc_patterns/src/list/base/list_states.dart';
 import 'package:flutter_bloc_patterns/src/list/base/repository.dart';
+import 'package:flutter_bloc_patterns/src/list/filter/filter_list_bloc.dart';
+import 'package:flutter_bloc_patterns/src/list/filter/filter_repository.dart';
 
-class ListBloc<T> extends Bloc<ListEvent, ListState> {
-  final Repository<T> _repository;
+class ListBloc<T> extends FilterListBloc<T, void> {
+  ListBloc(Repository<T> repository)
+      : assert(repository != null),
+        super(_FilterRepositoryAdapter(repository));
+}
 
-  ListBloc(Repository repository)
-      : this._repository = repository,
-        assert(repository != null);
+class _FilterRepositoryAdapter<T> extends FilterRepository<T, void> {
+  final Repository<T> repository;
+
+  _FilterRepositoryAdapter(this.repository);
 
   @override
-  ListState get initialState => ListLoading();
-
-  void loadItems() => dispatch(LoadList());
-
-  void refreshItems() => dispatch(RefreshList());
+  Future<List<T>> getAll() => repository.getAll();
 
   @override
-  Stream<ListState> mapEventToState(ListEvent event) async* {
-    if (event is LoadList) {
-      yield* _mapLoadList();
-    } else if (_isRefreshPossible(event)) {
-      yield* _mapRefreshList();
-    }
-  }
-
-  bool _isRefreshPossible(ListEvent event) =>
-      event is RefreshList &&
-          currentState is! ListLoading &&
-          currentState is! ListRefreshing;
-
-  Stream<ListState> _mapLoadList() async* {
-    yield ListLoading();
-    yield* _getItemsFromRepository();
-  }
-
-  Stream<ListState> _mapRefreshList() async* {
-    final listItems = _getCurrentStateItems();
-    yield ListRefreshing(listItems);
-    yield* _getItemsFromRepository();
-  }
-
-  List<T> _getCurrentStateItems() =>
-      (currentState is ListLoaded) ? (currentState as ListLoaded).items : [];
-
-  Stream<ListState> _getItemsFromRepository() async* {
-    try {
-      final List<T> items = await _repository.getAll();
-      yield items.isNotEmpty
-          ? ListLoaded(UnmodifiableListView(items))
-          : ListLoadedEmpty();
-    } catch (e) {
-      yield ListNotLoaded(e);
-    }
-  }
+  Future<List<T>> getBy(void filter) => repository.getAll();
 }
