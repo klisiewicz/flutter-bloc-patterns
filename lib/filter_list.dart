71caf906fc638108aff1af529f15dcec6d819fa8
klisiewicz <karol.lisiewicz@gmail.com>
2019-11-27 13:56:18 +0100

    ViewState exported.

--- lib/filter_list.dart
+++ lib/filter_list.dart
@@ -1,4 +1,5 @@
 export 'package:flutter_bloc_patterns/src/common/refresh_view.dart';
+export 'package:flutter_bloc_patterns/src/common/view_state.dart';
 export 'package:flutter_bloc_patterns/src/common/view_state_builder.dart';
 export 'package:flutter_bloc_patterns/src/list/filter/filter_list_bloc.dart';
 export 'package:flutter_bloc_patterns/src/list/filter/filter_list_repository.dart';
