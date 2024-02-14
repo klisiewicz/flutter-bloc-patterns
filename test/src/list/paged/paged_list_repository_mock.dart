bb1d628d72c9c26f2b27e444cf43a523f813c285
klisiewicz <klisiewicz@users.noreply.github.com>
2024-02-14 22:12:43 +0100

    Cleaned up page_list_blog_test.dart.

--- test/src/list/paged/paged_list_repository_mock.dart
+++ test/src/list/paged/paged_list_repository_mock.dart
@@ -4,7 +4,9 @@ import 'package:flutter_bloc_patterns/paged_list.dart';
 class InMemoryPagedListRepository<T> implements PagedListRepository<T> {
   final List<T> items;
 
-  InMemoryPagedListRepository(this.items);
+  InMemoryPagedListRepository([
+    this.items = const [],
+  ]);
 
   @override
   Future<List<T>> getAll(Page page) async {
